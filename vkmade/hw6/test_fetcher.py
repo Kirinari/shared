9f9dba6a1ad77547c79e20a9234c420433d9dbc5
AlexeySomov <kirinari@bk.ru>
2022-12-18 16:50:47 +0300

    .

--- vkmade/hw6/test_fetcher.py
+++ vkmade/hw6/test_fetcher.py
@@ -4,14 +4,14 @@ import url_maker
 from fetcher import fetch_url
 
 class TestFetch(IsolatedAsyncioTestCase):
-    # def setUp(self) -> None:
-    #     self.path = url_maker.PATH
-    #     self.n_links = url_maker.N_LINKS
+    def setUp(self) -> None:
+        self.path = url_maker.PATH
+        self.n_links = url_maker.N_LINKS
 
     def test_get_fetch_results(self):
         TEST_WORKERS = self.n_links // 1
         TEST_PATH = self.path
-        with mock.patch("fetcher.fetch_url.sessio.get") as m_get:
+        with mock.patch("fetcher.fetch_url.session.get") as m_get:
             m_get.return_value = mock.Mock(read="some data", status="200")
             print(m_get.call_count)
 
