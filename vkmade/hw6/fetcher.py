a7600a4ab34fdbe5008385feeb175de40e01327d
Kirinari <kirinari@bk.ru>
2022-12-18 14:36:34 +0300

    copied hw6 from local ubuntu

---
+++ vkmade/hw6/fetcher.py
@@ -0,0 +1,73 @@
+import aiohttp
+import asyncio
+import time
+import sys
+import argparse
+
+DEFAULT_WORKERS = 50
+
+async def fetch_url(url, session):
+    print(1)
+    async with session.get(url) as resp:
+        data = await resp.read()
+        print(resp.status, len(data))
+
+        return len(data) 
+    prin(2)
+
+
+async def worker(queue, session):
+    while True:
+        url = await queue.get()
+        try:
+            res = await fetch_url(url, session)
+        finally:
+            queue.task_done()
+
+
+async def fetch_batch_urls(queue, workers):
+    async with aiohttp.ClientSession() as session:
+        tasks = [
+            asyncio.create_task(worker(queue, session))
+            for i in range(workers)
+        ]
+
+        await queue.join()
+        for task in tasks:
+            task.cancel()
+
+
+async def main(filepath, workers):
+    with open(filepath, 'r') as f:
+        urls_queue = asyncio.Queue()
+        for new_url in f:
+            await urls_queue.put(new_url)
+        await fetch_batch_urls(urls_queue, workers)
+
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('-c', '--count', nargs=1, default=DEFAULT_WORKERS, type=int, required=True)
+    parser.add_argument('filepath')
+    parser.add_argument('-t', '--time', action='store_const', const=True)
+ 
+    return parser
+
+
+if __name__ == "__main__":
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+    filepath = namespace.filepath
+    WORKERS = namespace.count[0]
+
+
+    if namespace.time:
+        t1 = time.time()
+
+    asyncio.run(main(filepath, WORKERS))
+
+    if namespace.time:
+        print(time.time() - t1)
+
+
---
+++ vkmade/hw6/test_fetcher.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest import mock, IsolatedAsyncioTestCase
+import url_maker
+from fetcher import fetch_url
+
+class TestFetch(IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.path = url_maker.PATH
+        self.n_links = url_maker.N_LINKS
+
+    def test_get_fetch_results(self):
+        TEST_WORKERS = self.n_links // 1
+        TEST_PATH = self.path
+        with mock.patch("fetcher.fetch_url.sessio.get") as m_get:
+            m_get.return_value = mock.Mock(read="some data", status="200")
+            print(m_get.call_count)
+
+
+
+
